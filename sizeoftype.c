fd62c076a489bf937313aead5c58ff10e059cf38
Bostanova <kontakt.bostanova@yandex.ru>
2020-12-03 14:33:00 +0300

    add sizeoftype.c

---
+++ sizeoftype.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+    // printf("values from standard headers\n\n");
+    // printf("signed char: %d   %d\n", SCHAR_MIN, SCHAR_MAX);
+    // printf("unsigned char: %d   %d\n", 0, UCHAR_MAX);
+    // printf("signed int: %d   %d\n", INT_MIN, INT_MAX);
+    // printf("signed int: %d   %d\n", INT_MIN, INT_MAX);
+
+    // int integer = 1, 
+    // i = 0;
+    // while(integer > 0)
+    // { 
+    //     integer = integer*2; 
+    //     i++; 
+    // }
+    // printf("[%d,%d]\n", INT_MIN, INT_MAX);
+    // printf("[%d,%d]\n sum iteration: %d\n", integer, integer-1, i);
+    // printf("%d\n", sizeof(int));
+    // int j = 2147483647;
+    // printf("%d\n", (j+1));
+
+    char *s = "Hello";
+    printf("%d\n", sizeof(s));
+
+    return 0;
+}
