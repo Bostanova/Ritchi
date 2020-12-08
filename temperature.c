8f7f98c50accbc509222c93900ab5c85eb9ac5c6
Elenor Erika <eerika@oa-l5.msk.21-school.ru>
2020-12-08 14:22:55 +0300

    fix temperature.c

--- temperature.c
+++ temperature.c
@@ -5,18 +5,18 @@
 
 int main()
 {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;
     upper = 300;
     step = 20;
-
     fahr = lower;
+    printf("fahr\t celsius\n");
     while (fahr <= upper)
     {
         celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t %d\n", fahr, celsius);
+        printf("%3.0f\t %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
